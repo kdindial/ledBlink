bd86bd42f39bbb7c077d6aa66541a0429194c336
kdindial <knd286@nyu.edu>
2020-12-04 13:23:28 -0500

    Update ledBlink.ino
    Made the ledBlink

--- ledBlink.ino
+++ ledBlink.ino
@@ -1,9 +1,11 @@
+int ledPin=13
 void setup() {
-  // put your setup code here, to run once:
-//
+pinMode(13,OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(ledPin,HIGH);
+  delay(1000);
+  digitalWrite(ledPin,LOW);
+  delay(1000);
 }
